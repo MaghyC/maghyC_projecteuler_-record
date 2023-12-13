239f606273831e64d464cfddd2eea91347d5d4f3
MaghyC <129014665+MaghyC@users.noreply.github.com>
2023-12-13 12:35:17 +0800

    Update myTool.py

--- myTool.py
+++ myTool.py
@@ -3,6 +3,26 @@ import math
 
 
 # tools    
+def factorization(n):
+  if n<2:
+    return {}
+  
+  i = 2
+  k=0
+  dict={}
+  while i < n/2+1:
+    if n%i==0:
+      dict.update({f'{i}': 0}) 
+      temp_n_b=int(math.log(n,i)+1)
+      for _ in range(1, temp_n_b  ):
+        if n%i==0:
+          n/=i
+          dict[f'{i}']=dict[f'{i}']+1
+    i+=1
+  if dict == {}:
+    dict={f'{n}':1}
+  return dict
+  
 def is_Prime_Lis(lis, n):
   if n <= 1:
     return False
@@ -103,6 +123,31 @@ def n_digit_largest_product_in_series(n,series_init):
     i+=1
     k+=1
   return max_product
+
+#Problem_9
+def find_a_b_c_eq_n(n):
+  
+  c_xia=int(n/3)
+  
+  sq=np.array([])
+  alis=np.array([])
+  for c in range(1,int(n/2+1)):
+    c_2=c**2
+    alis=np.append(alis,c)
+    sq=np.append(sq,c_2)
+    abc=None
+    if c> c_xia:
+      for i in range(len(sq)):
+          for j in range(i+1,len(sq)):
+            if (sq[i]+sq[j])==c_2 and alis[i]+alis[j]+c==n:
+              abc=alis[i]*alis[j]*c
+
+return abc
+
+#Problem_10
+#sum(findPrimeIn(n))
+
+
     
 
 
